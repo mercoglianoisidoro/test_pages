ae9f8dd6373b4d4436ba89b79d9440c4b3c5dace
Mercogliano Isidoro <mercogliano.isidoro@gmail.com>
2020-09-20 16:24:43 +0200

    test

--- serve.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm \
- --volume="$PWD:/srv/jekyll" \
- --env JEKYLL_ENV=development -p 4000:4000 jekyll/jekyll bundle install && jekyll serve
-
-#  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
-
-# :3.8
